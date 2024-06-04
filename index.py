cc4f62e728825c8117d321448282e45c55f7a202
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-06-04 20:31:44 -0300

    Test

---
+++ index.py
@@ -0,0 +1,15 @@
+import requests
+import json   #biblioteca que transforma um dicionário no json
+
+link = "https://webhook.site/70d97d9e-20a3-41bb-ab54-8002a1046029"
+
+
+dicionario = {
+    "nome": "Eduarda",
+    "valor": 76,
+    "parcelas": 3,
+}
+
+# Convertendo o dicionário em JSON
+dicionario_ajustado = json.dumps(dicionario)
+requests.post(link, data=dicionario_ajustado)
